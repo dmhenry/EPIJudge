fd7120756e00b712d8b56e780cb7d749618fe5f7
dmhenry <davermont@gmail.com>
2021-03-08 17:22:18 -0500

    Solutions to first two string problems.

--- epi_judge_java/epi/ConvertBase.java
+++ epi_judge_java/epi/ConvertBase.java
@@ -1,19 +1,60 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ConvertBase {
-  @EpiTest(testDataFile = "convert_base.tsv")
+    @EpiTest(testDataFile = "convert_base.tsv")
 
-  public static String convertBase(String numAsString, int b1, int b2) {
-    // TODO - you fill in here.
-    return "";
-  }
+    public static String convertBase(String numAsString, int b1, int b2) {
+        StringBuilder result = new StringBuilder();
+        long decimal = toDecimal(numAsString, b1);
+        boolean isNegative = false;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ConvertBase.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        if (decimal == 0)
+            return "0";
+
+        if (decimal < 0) {
+            decimal *= -1;
+            isNegative = true;
+        }
+
+        while (decimal > 0) {
+            long d = decimal % b2;
+            if (d < 10) {
+                result.append(d);
+            } else {
+                result.append((char) ('A' + d - 10));
+            }
+            decimal /= b2;
+        }
+
+        if (isNegative) {
+            result.append('-');
+        }
+        return result.reverse().toString();
+    }
+
+    private static long toDecimal(String numAsString, int radix) {
+        long decimal = 0;
+        int sign = (numAsString.startsWith("-")) ? -1 : 1;
+        int digit;
+        int ch;
+
+        for (int i = (sign == -1) ? 1 : 0; i < numAsString.length(); i++) {
+            ch = numAsString.charAt(i);
+            digit = Character.isDigit(ch) ? ch - '0' : Character.toUpperCase(ch) - 'A' + 10;
+            decimal = decimal * radix + digit;
+        }
+        return sign * decimal;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ConvertBase.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -1,32 +1,64 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
+
 public class StringIntegerInterconversion {
 
-  public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "0";
-  }
-  public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
-  }
-  @EpiTest(testDataFile = "string_integer_interconversion.tsv")
-  public static void wrapper(int x, String s) throws TestFailure {
-    if (Integer.parseInt(intToString(x)) != x) {
-      throw new TestFailure("Int to string conversion failed");
-    }
-    if (stringToInt(s) != x) {
-      throw new TestFailure("String to int conversion failed");
-    }
-  }
+    public static String intToString(int x) {
+        if (x == 0)
+            return "0";
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "StringIntegerInterconversion.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        StringBuilder result = new StringBuilder();
+        long num = x;
+        if (num < 0) {
+            num *= -1;
+        }
+
+        while (num > 0) {
+            result.append(num % 10);
+            num /= 10;
+        }
+
+        if (x < 0) {
+            result.append("-");
+        }
+        return result.reverse().toString();
+    }
+
+    public static int stringToInt(String s) {
+        int sign = 1;
+        long digits = 0;    // Avoid overflow when s = "-2147483648"
+
+        if (s.charAt(0) == '-') {
+            sign = -1;
+        } else if (s.charAt(0) != '+') {
+            digits = s.charAt(0) - '0';
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            digits = digits * 10 + (s.charAt(i) - '0');
+        }
+        return (int) (sign * digits);
+    }
+
+    @EpiTest(testDataFile = "string_integer_interconversion.tsv")
+    public static void wrapper(int x, String s) throws TestFailure {
+        if (Integer.parseInt(intToString(x)) != x) {
+            throw new TestFailure("Int to string conversion failed");
+        }
+        if (stringToInt(s) != x) {
+            throw new TestFailure("String to int conversion failed");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "StringIntegerInterconversion.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
