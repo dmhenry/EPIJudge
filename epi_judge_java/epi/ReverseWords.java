ffbb70d30171058c8993b87929bd02c270f0055b
dmhenry <davermont@gmail.com>
2021-03-09 06:09:28 -0500

    Solutions for 4 string problems

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -1,28 +1,47 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
 public class ReverseWords {
 
-  public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
-    return;
-  }
-  @EpiTest(testDataFile = "reverse_words.tsv")
-  public static String reverseWordsWrapper(TimedExecutor executor, String s)
-      throws Exception {
-    char[] sCopy = s.toCharArray();
+    public static void reverseWords(char[] input) {
+        int left = 0, right = 0;
+        while (left < input.length) {
+            while (right < input.length && !Character.isSpaceChar(input[right]))
+                right++;
 
-    executor.run(() -> reverseWords(sCopy));
+            reverse(input, left, right - 1);
+            left = ++right;
+        }
+        reverse(input, 0, input.length - 1);
+    }
 
-    return String.valueOf(sCopy);
-  }
+    private static void reverse(char[] input, int left, int right) {
+        while (left < right) {
+            char temp = input[left];
+            input[left++] = input[right];
+            input[right--] = temp;
+        }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseWords.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    @EpiTest(testDataFile = "reverse_words.tsv")
+    public static String reverseWordsWrapper(TimedExecutor executor, String s)
+            throws Exception {
+        char[] sCopy = s.toCharArray();
+
+        executor.run(() -> reverseWords(sCopy));
+
+        return String.valueOf(sCopy);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseWords.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/SpreadsheetEncoding.java
+++ epi_judge_java/epi/SpreadsheetEncoding.java
@@ -1,19 +1,25 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SpreadsheetEncoding {
-  @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
-  public static int ssDecodeColID(final String col) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
+    public static int ssDecodeColID(final String col) {
+        int id = 0;
+        for (int i = 0; i < col.length(); i++) {
+            id = id * 26 + col.charAt(i) - 'A' + 1;
+        }
+        return id;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SpreadsheetEncoding.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SpreadsheetEncoding.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
