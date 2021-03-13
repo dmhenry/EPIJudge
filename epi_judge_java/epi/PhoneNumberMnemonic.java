7ff07f55e09fced06f6250bfb5cf8c33ae9467af
dmhenry <davermont@gmail.com>
2021-03-13 14:43:02 -0500

    Solution for PhoneNumberMnemonic

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -1,33 +1,69 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.BiPredicate;
+import java.util.Map;
+
 public class PhoneNumberMnemonic {
-  @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
 
-  public static List<String> phoneMnemonic(String phoneNumber) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<String> expected, List<String> result) {
-    if (result == null) {
-      return false;
+    private static final Map<Character, List<Character>> charsByNum = new HashMap<>();
+
+    static {
+        charsByNum.put('0', Collections.singletonList('0'));
+        charsByNum.put('1', Collections.singletonList('1'));
+        charsByNum.put('2', Arrays.asList('A', 'B', 'C'));
+        charsByNum.put('3', Arrays.asList('D', 'E', 'F'));
+        charsByNum.put('4', Arrays.asList('G', 'H', 'I'));
+        charsByNum.put('5', Arrays.asList('J', 'K', 'L'));
+        charsByNum.put('6', Arrays.asList('M', 'N', 'O'));
+        charsByNum.put('7', Arrays.asList('P', 'Q', 'R', 'S'));
+        charsByNum.put('8', Arrays.asList('T', 'U', 'V'));
+        charsByNum.put('9', Arrays.asList('W', 'X', 'Y', 'Z'));
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PhoneNumberMnemonic.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
+    public static List<String> phoneMnemonic(String phoneNumber) {
+        List<String> results = new ArrayList<>();
+        addMnemonicRecur(phoneNumber, 0, "", results);
+        return results;
+    }
+
+    private static void addMnemonicRecur(String phoneNumber, int i, String mnemonicPrefix, List<String> results) {
+        char digit = phoneNumber.charAt(i);
+        List<Character> keyLetters = charsByNum.get(digit);
+
+        for (Character letter : keyLetters) {
+            if (i == phoneNumber.length() - 1) {
+                results.add(mnemonicPrefix + letter);
+            } else {
+                addMnemonicRecur(phoneNumber, i + 1, mnemonicPrefix + letter, results);
+            }
+        }
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<String> expected, List<String> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PhoneNumberMnemonic.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
