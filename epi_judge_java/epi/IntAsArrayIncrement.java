c68a4483c1c262a22b1bcb8b2c403436c0e1fba2
dmhenry <davermont@gmail.com>
2021-03-15 15:22:15 -0400

    Solve Dutch National Flag; Increment Integer as List

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -1,20 +1,34 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
-public class IntAsArrayIncrement {
-  @EpiTest(testDataFile = "int_as_array_increment.tsv")
-  public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntAsArrayIncrement.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class IntAsArrayIncrement {
+    
+    @EpiTest(testDataFile = "int_as_array_increment.tsv")
+    public static List<Integer> plusOne(List<Integer> A) {
+        boolean carry = true;
+        for (int i = A.size() - 1; i >= 0 && carry; i--) {
+            Integer digit = A.get(i);
+            if (digit < 9) {
+                carry = false;
+            }
+            A.set(i, (digit + 1) % 10);
+        }
+        if (carry) {
+            A.add(0, 1);
+        }
+        return A;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntAsArrayIncrement.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
