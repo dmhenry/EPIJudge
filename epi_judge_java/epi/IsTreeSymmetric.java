9ea9c8c3ba6eb48c0ac8cd6715cb418bd9f7bb5b
dmhenry <davermont@gmail.com>
2021-04-25 10:16:17 -0400

    Solve binary tree is symmetric

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,19 +1,26 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeSymmetric {
-  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
-  public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_tree_symmetric.tsv")
+    public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
+        return (tree == null) || isSymmetric(tree.left, tree.right);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeSymmetric.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static boolean isSymmetric(BinaryTreeNode<Integer> a, BinaryTreeNode<Integer> b) {
+        return (a == null && b == null) || (a != null && b != null && a.data.equals(b.data)
+                && isSymmetric(a.left, b.right) && isSymmetric(a.right, b.left));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeSymmetric.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
