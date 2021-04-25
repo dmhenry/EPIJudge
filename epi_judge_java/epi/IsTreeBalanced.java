aad05a9183fcee60eea0489f288fbffc548e811c
dmhenry <davermont@gmail.com>
2021-04-25 10:02:14 -0400

    Solve height balanced binary tree

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,37 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
+        return (balanceHeight(tree) >= 0);
+    }
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    private static int balanceHeight(BinaryTreeNode<Integer> tree) {
+        if (tree == null)
+            return 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int leftHeight = balanceHeight(tree.left);
+        if (leftHeight < 0)
+            return leftHeight;
+
+        int rightHeight = balanceHeight(tree.right);
+        if (rightHeight < 0)
+            return rightHeight;
+
+        return (Math.abs(leftHeight - rightHeight) > 1) ? -1 : 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeBalanced.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
+
 }
