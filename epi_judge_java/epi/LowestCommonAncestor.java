941823d2a59d6f2ff4313e8d39593b440a0d78ab
dmhenry <davermont@gmail.com>
2021-04-28 21:07:46 -0400

    Solve Lowest Common Ancestor naive

--- epi_judge_java/epi/LowestCommonAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestor.java
@@ -1,37 +1,78 @@
 package epi;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LowestCommonAncestor {
-  public static BinaryTreeNode<Integer> lca(BinaryTreeNode<Integer> tree,
-                                            BinaryTreeNode<Integer> node0,
-                                            BinaryTreeNode<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
-  public static int lcaWrapper(TimedExecutor executor,
-                               BinaryTreeNode<Integer> tree, Integer key0,
-                               Integer key1) throws Exception {
-    BinaryTreeNode<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
-    BinaryTreeNode<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
 
-    BinaryTreeNode<Integer> result =
-        executor.run(() -> lca(tree, node0, node1));
+    public static BinaryTreeNode<Integer> lca(BinaryTreeNode<Integer> tree,
+                                              BinaryTreeNode<Integer> node0,
+                                              BinaryTreeNode<Integer> node1) {
 
-    if (result == null) {
-      throw new TestFailure("Result can not be null");
+        List<BinaryTreeNode<Integer>> path0 = findPath(tree, node0, new ArrayList<>());
+        List<BinaryTreeNode<Integer>> path1 = findPath(tree, node1, new ArrayList<>());
+
+        List<BinaryTreeNode<Integer>> shorter = (path1.size() < path0.size()) ? path1 : path0;
+        BinaryTreeNode<Integer> result = tree;
+
+        int i;
+        for (i = 1; i < shorter.size(); i++) {
+            if (!path0.get(i).equals(path1.get(i))) {
+                result = path0.get(i - 1);
+                break;
+            }
+        }
+        if (i == shorter.size()) {
+            result = shorter.get(i - 1);
+        }
+        return result;
     }
-    return result.data;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LowestCommonAncestor.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static List<BinaryTreeNode<Integer>> findPath(BinaryTreeNode<Integer> tree, BinaryTreeNode<Integer> key, List<BinaryTreeNode<Integer>> path) {
+        if (tree == null)
+            return null;
+
+        path.add(tree);
+
+        if (key.getData().equals(tree.getData())) {
+            return path;
+        }
+
+        List<BinaryTreeNode<Integer>> found = findPath(tree.getLeft(), key, new ArrayList<>(path));
+        if (found == null) {
+            found = findPath(tree.getRight(), key, new ArrayList<>(path));
+        }
+        return found;
+    }
+
+    @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
+    public static int lcaWrapper(TimedExecutor executor,
+                                 BinaryTreeNode<Integer> tree, Integer key0,
+                                 Integer key1) throws Exception {
+        BinaryTreeNode<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
+        BinaryTreeNode<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
+
+        BinaryTreeNode<Integer> result =
+                executor.run(() -> lca(tree, node0, node1));
+
+        if (result == null) {
+            throw new TestFailure("Result can not be null");
+        }
+        return result.data;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "LowestCommonAncestor.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
