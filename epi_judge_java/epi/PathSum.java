741bba2c0bd7c908cb376070b4b1e0ce8a151f9c
dmhenry <davermont@gmail.com>
2021-04-30 16:30:06 -0400

    Solve path sum

--- epi_judge_java/epi/PathSum.java
+++ epi_judge_java/epi/PathSum.java
@@ -1,20 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class PathSum {
-  @EpiTest(testDataFile = "path_sum.tsv")
 
-  public static boolean hasPathSum(BinaryTreeNode<Integer> tree,
-                                   int remainingWeight) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "path_sum.tsv")
+    public static boolean hasPathSum(BinaryTreeNode<Integer> tree, int remainingWeight) {
+        if (tree == null) {
+            return false;
+        } else if (tree.getLeft() == null && tree.getRight() == null) {
+            return tree.getData() == remainingWeight;
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PathSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        remainingWeight = remainingWeight - tree.getData();
+        return hasPathSum(tree.getLeft(), remainingWeight) || hasPathSum(tree.getRight(), remainingWeight);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PathSum.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
