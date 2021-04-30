2474619294acc0cb37a3d9c79ec8e5990f10892f
dmhenry <davermont@gmail.com>
2021-04-29 21:52:51 -0400

    Solve sum root to leaf

--- epi_judge_java/epi/SumRootToLeaf.java
+++ epi_judge_java/epi/SumRootToLeaf.java
@@ -1,19 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
-  public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
+    public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
+        return sumRootToLeaf(tree, 0);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SumRootToLeaf.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int sumRootToLeaf(BinaryTreeNode<Integer> tree, int partialSum) {
+        if (tree == null)
+            return 0;
+
+        partialSum = 2 * partialSum + tree.getData();
+
+        if (tree.left == null && tree.right == null)
+            return partialSum;
+
+        return sumRootToLeaf(tree.left, partialSum) + sumRootToLeaf(tree.right, partialSum);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SumRootToLeaf.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
